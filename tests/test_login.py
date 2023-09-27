6fda5a93a55c443aa4ae4efec9753027d6e421bc
Balaji <balaji0017@gmail.com>
2023-09-27 10:38:08 +0530

    data driven using pytest params

--- tests/test_login.py
+++ tests/test_login.py
@@ -12,16 +12,20 @@ from base.appium_wrapper import AutomationWrapper
 
 class TestLogin(AutomationWrapper):
 
-    def test_invalid_login(self):
+    @pytest.mark.parametrize("username,password,expected_error",[
+        ('john123@gmail.com','john123','Incorrect email or password'),
+        ('pete123@gmail.com', 'pete123', 'Incorrect email or password')
+    ])
+    def test_invalid_login(self,username,password,expected_error):
         time.sleep(3)
         self.driver.find_element(AppiumBy.NAME, 'Sign In').click()
-        self.driver.find_element(AppiumBy.XPATH, "//Edit[@Name='Enter your email']").send_keys("hello12345678@gmail.com")
+        self.driver.find_element(AppiumBy.XPATH, "//Edit[@Name='Enter your email']").send_keys(username)
         self.driver.find_element(AppiumBy.XPATH, "//Edit[@Name='Enter your password']").click()
-        self.driver.find_element(AppiumBy.XPATH, "//Edit[@Name='Enter your password']").send_keys("hello123")
+        self.driver.find_element(AppiumBy.XPATH, "//Edit[@Name='Enter your password']").send_keys(password)
         self.driver.find_element(AppiumBy.XPATH, '//Button[@Name="Sign In"]').click()
         time.sleep(10)
         actual_error = self.driver.find_element(AppiumBy.XPATH, "//*[contains(@Name,'Incorrect')]").get_attribute("Name")
-        assert_that(actual_error).is_equal_to("Incorrect email or password")
+        assert_that(actual_error).is_equal_to(expected_error)
 
     def test_valid_login(self):
         self.driver.find_element(AppiumBy.NAME, 'Sign In').click()
